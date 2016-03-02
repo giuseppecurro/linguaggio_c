c828ff07aa0ddf5fa561ff23c40475bb50dbd4e4
giuseppecurro <giuseppe.curro1@gmail.com>
2016-03-02 19:10:39 +0100

    primo lotto file

---
+++ README.md
@@ -0,0 +1,2 @@
+# socket
+codici sperimentali
---
+++ prova_stringhe/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nome[5];
+    printf("Nome: ");
+    scanf("%s",&nome);
+    printf("%s",nome);
+    return 0;
+}
